6a069ec811e89cb7c00c7771acbcbb2f05540f07
liqiang <liqiang_wuxi@163.com>
2015-08-07 17:08:42 +0800

    。
    。

---
+++ 朝阳能源结算/朝阳能源--代码/朝阳能源结算/Classes/Account(账户户头)/Controller/LQAccountVC.h
@@ -0,0 +1,13 @@
+//
+//  LQAccountVC.h
+//  朝阳能源结算
+//
+//  Created by admin on 15/8/7.
+//  Copyright (c) 2015年 dieshang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LQAccountVC : UIViewController
+
+@end
---
+++ 朝阳能源结算/朝阳能源--代码/朝阳能源结算/Classes/Account(账户户头)/Controller/LQAccountVC.m
@@ -0,0 +1,29 @@
+//
+//  LQAccountVC.m
+//  朝阳能源结算
+//
+//  Created by admin on 15/8/7.
+//  Copyright (c) 2015年 dieshang. All rights reserved.
+//
+
+#import "LQAccountVC.h"
+
+@interface LQAccountVC ()
+
+@end
+
+@implementation LQAccountVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self doLoading];
+}
+
+- (void)doLoading
+{
+
+}
+
+@end
