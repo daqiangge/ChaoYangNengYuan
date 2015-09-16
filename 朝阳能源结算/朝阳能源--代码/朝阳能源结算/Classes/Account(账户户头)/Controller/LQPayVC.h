cffe4b29690d8602674b18a7990f50e182a25bd9
liqiang <liqiang_wuxi@163.com>
2015-09-16 17:34:10 +0800

    .
    .

---
+++ 朝阳能源结算/朝阳能源--代码/朝阳能源结算/Classes/Account(账户户头)/Controller/LQPayVC.h
@@ -0,0 +1,17 @@
+//
+//  LQPayVC.h
+//  朝阳能源结算
+//
+//  Created by admin on 15/9/16.
+//  Copyright (c) 2015年 dieshang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LQAccountItemModel;
+
+@interface LQPayVC : UIViewController
+
+@property (nonatomic, strong) LQAccountItemModel *accountItemModel;
+
+@end
